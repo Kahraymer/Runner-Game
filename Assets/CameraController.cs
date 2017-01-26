cd958d77b4bd691dfac877be5be33419cbe491a6
Kahraymer <tomadamkremer@gmail.com>
2017-01-26 13:23:38 -0800

    Added camera tracking option
    Added a commented out line of code that would make the camera not
    follow player jumps

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -18,5 +18,8 @@ public class CameraController : MonoBehaviour {
 	// Update is called once per frame
 	void LateUpdate () {
 		transform.position = player.transform.position + offset;
+
+		// If you don't want the camera to follow jumps, use the line below instead:
+		// transform.position = new Vector3 (player.transform.position.x, 0, 0) + offset;
 	}
 }
