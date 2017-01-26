1426fc3093ba36aece0e1cefa892d19debacee8b
Kahraymer <tomadamkremer@gmail.com>
2017-01-26 13:14:09 -0800

    Added Camera Controller Script
    Added camera script to have player stay on left, follows player across
    map. For now, follows on jumps too

---
+++ Assets/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	public GameObject background;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position + new Vector3(background.transform.position.x, -background.transform.position.y*3/4,background.transform.position.z) + player.transform.position/2;
+	}
+
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
