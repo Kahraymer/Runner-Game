4a6f55569fe2a444eacc0714ad14c9567404fc1d
matthewmau <mmau@stanford.edu>
2017-02-04 20:34:30 -0800

    WorldBaseM
    Modification of worldbase script: simplifcation, use of separate level
    script to extend its functionality

---
+++ Assets/Matthew/LevelM1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelM1 : WorldBaseM {
+
+    public GameObject greendirt;
+    public GameObject column;
+    private int column_y = 5;
+
+
+	// Use this for initialization
+	void Start () {
+      //  pc = Instantiate( Quaternion.identity)
+        addGround();
+        addColumns();
+        WorldUpdate();
+    }
+
+    private void addGround () {
+        for (int i = 0; i < 250; i++) {
+            levelObjects.Add(makeWorldEntry(new Vector3(i, 0, 0), greendirt));
+        }
+    }
+
+    private void addColumns () {
+        for (int i = 0; i < 250; i++) {
+            if (i % 10 == 0) {
+                levelObjects.Add(makeWorldEntry(new Vector3(i, column_y, 0), column));
+            }
+
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		WorldUpdate ();
+	}
+}
