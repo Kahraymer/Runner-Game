ef47cc6ea8419f118031b506e77e3c749d6b1b36
Kahraymer <tomadamkremer@gmail.com>
2017-02-04 17:31:36 -0800

    Got WorldBase working
    WorldBase works if attached to world object in scene. Haven’t yet
    figured out how to transfer info from a level’s custom script (the list
    that represents the level) to world base.

--- Assets/PC Assets/BasicRunning.cs
+++ Assets/PC Assets/BasicRunning.cs
@@ -12,19 +12,22 @@ public class BasicRunning : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         // Continue to run to the right.
-        pcRigidbody2D = GetComponent<Rigidbody2D>();
-
-        
+       // pcRigidbody2D = GetComponent<Rigidbody2D>();
+//		pcRigidbody2D.velocity = new Vector2(startingSpeed, pcRigidbody2D.velocity.y); 
         //GetComponent<Rigidbody2D>().AddForce(Vector2.right * startingSpeed, ForceMode2D.Force);
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        // transform.Translate(startingSpeed * Time.deltaTime, 0f, 0f);
-
+       // transform.Translate(startingSpeed * Time.deltaTime, 0f, 0f);
+//		float xpos = transform.position.x;
+//		transform.position.x = (xpos + startingSpeed);
+		Vector3 change = new Vector3(startingSpeed, 0, 0);
+		//transform.position = new Vector3 (transform.position.x + startingSpeed, transform.position.y, transform.position.z);
+		transform.position += change;
 		// TODO: Is this making a new velocity vector every time? 
-        pcRigidbody2D.velocity = new Vector2(startingSpeed, pcRigidbody2D.velocity.y);
-
-    }
+//		pcRigidbody2D.velocity = new Vector2(startingSpeed, pcRigidbody2D.velocity.y); 
+		//transform.Translate(5f * Time.deltaTime, 0f, 0f);
+	}
 }
