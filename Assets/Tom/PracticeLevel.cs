a5b057cb2da1adc76e1e1f9d008358cf0cb0de24
Kahraymer <tomadamkremer@gmail.com>
2017-02-05 20:59:28 -0800

    Got ground spawning working

--- Assets/Tom/PracticeLevel.cs
+++ Assets/Tom/PracticeLevel.cs
@@ -5,31 +5,32 @@ using UnityEngine;
 public class PracticeLevel : MonoBehaviour {
 
 	public GameObject ground;
+//	public GameObject level_pc;
 //	public GameObject mypc;
 //	public GameObject coin;
 
 
 	// Use this for initialization
 	void Start () {
+		Debug.Log ("PRACTICELEVEL START");
 
-//		WorldBase wb = GetComponent<WorldBase> ();
-//
-//		float blockWidth = GetComponent<BoxCollider2D> ().bounds.size.x;
-//
-//		wb.levelObjects.Clear();
-//
-//
-//		WorldBase.WorldEntry entry = new WorldBase.WorldEntry ();
-//
-//		entry.loc = new Vector3 (4, 0, 0);
-//		entry.obj = Instantiate(ground);
-//		wb.levelObjects.Add (entry);
-//
-//		wb.WorldStart ();
+		WorldBase wb = GetComponent<WorldBase> ();
 
-//		WorldStart ();
+		List<WorldBase.WorldEntry> thisLevel = new List<WorldBase.WorldEntry> ();
 
-//		WorldUpdate ();
+		for (int i = 0; i < 80; i++) {
+			WorldBase.WorldEntry entry = new WorldBase.WorldEntry ();
+
+			float blockWidth = ground.GetComponent<BoxCollider2D> ().size.x; //   bounds.size.x;
+			Debug.Log ("BLOCKWIDTH IS " + blockWidth);
+			Debug.Log ("i * blockWidth is " + i*blockWidth);
+			entry.loc = new Vector3 (4 + i*blockWidth, 0, 0);
+			entry.obj = ground;
+			thisLevel.Add (entry);
+		}
+
+		wb.WorldStart (thisLevel);
+
 
 	}
 	
