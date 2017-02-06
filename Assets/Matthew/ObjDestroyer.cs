1d7c4b8ce685833b74e41fa7d1b3ff6c30d1aa5c
Kahraymer <tomadamkremer@gmail.com>
2017-02-05 21:37:08 -0800

    Spawner works 100%
    WorldBase and the specific level’s script have to both be components of
    the world game object in the scene. If they are, the spawner works
    completely, including instantiating and deleting objects (and not
    losing the prefab after deleting it).

--- Assets/Matthew/ObjDestroyer.cs
+++ Assets/Matthew/ObjDestroyer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ObjDestroyer : MonoBehaviour {
 
     private GameObject pc;
-    private int outOfFrameOffset = 3;
+    private int outOfFrameOffset = 5;
 
 	// Use this for initialization
 	void Start () {
