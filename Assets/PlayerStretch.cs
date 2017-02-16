ec5e0fe669b996b0bd3f6a576509b24a7f127193
Varun Ramesh <varunramesh@live.com>
2017-02-15 22:02:14 -0800

    Stretch the player a bit when jumping.

---
+++ Assets/PlayerStretch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStretch : MonoBehaviour {
+
+	private Rigidbody2D rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float targetScale = 1.0f - 0.3f * Mathf.Clamp (rigidbody.velocity.y, 0.0f, 20.0f) / 20.0f;
+		transform.localScale = new Vector3 (targetScale , transform.localScale.y, transform.localScale.z);
+	}
+}
