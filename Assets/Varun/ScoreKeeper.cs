61dbcfdc26833082a025c4fe65b21e4ac5d62244
Varun Ramesh <varunramesh@live.com>
2017-02-15 21:52:37 -0800

    Pulsing score keeper.

--- Assets/Varun/ScoreKeeper.cs
+++ Assets/Varun/ScoreKeeper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEditor.Animations;
 
 public class ScoreKeeper : MonoBehaviour {
 
@@ -16,6 +17,7 @@ public class ScoreKeeper : MonoBehaviour {
 	public void AddScore(int scoreToAdd) {
 		score += scoreToAdd;
 		scoreText.GetComponent<Text> ().text = score.ToString().PadLeft (4, '0');
+		scoreText.GetComponent<Animator> ().SetTrigger("Pulse");
 	}
 	
 	// Update is called once per frame
