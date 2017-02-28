de67dcae737bbe0f8c3b462e17a55251b8f07fbe
Kahraymer <tomadamkremer@gmail.com>
2017-02-27 20:06:32 -0800

    Added death by falling
    Slight modification to health manager and player controller

--- Assets/ObstacleMovement.cs
+++ Assets/ObstacleMovement.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class ObstacleMovement : MonoBehaviour {
 
-	private Vector3 position;
 	private Rigidbody2D obstacle_rigid_body;
-//	private ground_level
 	public float speed = 5;
 	public AudioClip bumpSound;
 
@@ -15,15 +13,11 @@ public class ObstacleMovement : MonoBehaviour {
 	void Start () {
 		obstacle_rigid_body = GetComponent<Rigidbody2D> ();
 		obstacle_rigid_body.velocity = new Vector2 (0, -speed);
-//		ground_level = GameObject.FindGameObjectWithTag("Respawn");
-//		position = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-//		if (transform.position.y - GetComponent<BoxCollider2d> ().size.height/2 <= 
 		obstacle_rigid_body.velocity = new Vector2 (0, obstacle_rigid_body.velocity.y);
-//		transform.position = new Vector3 (position.x, position.y - (speed * Time.deltaTime), 0);
 	}
 
 	void OnTriggerEnter2D(Collider2D coll) {
