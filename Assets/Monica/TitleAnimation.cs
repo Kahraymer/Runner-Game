21248c6be6692aa1334b9d9f8918c76ead29bb4a
Monica Yupa <monica.yupa@gmail.com>
2017-02-28 20:39:19 -0800

    Adding animated title screen
    - Font for title
    - Animation script for title text
    - New start button appearance

---
+++ Assets/Monica/TitleAnimation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleAnimation : MonoBehaviour {
+
+	float letterTime = 0.1f;
+	string title;
+	Text screenTitle;
+
+	// Use this for initialization
+	void Start () {
+		screenTitle = GetComponent<Text>();
+		title += screenTitle.text;
+		screenTitle.text = "";
+		StartCoroutine(animateText());
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	IEnumerator animateText() {
+		foreach (char letter in title.ToCharArray()) {
+			//finalTitle += letter;
+			screenTitle.text += letter;
+			yield return new WaitForSeconds (letterTime);
+		}
+	}
+}
