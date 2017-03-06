06947e067c9aa9bd08977faad608cd4992a2eddf
Varun Ramesh <varunramesh@live.com>
2017-03-05 22:37:21 -0800

    Caves with scrolling background.

---
+++ Assets/ParallaxBackground.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour {
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition += Time.deltaTime * speed * new Vector3 (1, 0, 0);
+	}
+}
