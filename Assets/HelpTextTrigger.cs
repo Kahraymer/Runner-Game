60f75dbf79f525a6b513a2eb866e694e76f0b1ba
Varun Ramesh <varunramesh@live.com>
2017-03-08 17:59:15 -0800

    Timer for triggering help text.

---
+++ Assets/HelpTextTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpTextTrigger : MonoBehaviour {
+
+	public GameObject helpText;
+	public float visibleTime;
+
+	private float triggered = false;
+	private float visibleTimer = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (triggered) {
+			if (visibleTimer > visibleTime) {
+				helpText.SetActive (true);
+			} else {
+				visibleTimer += Time.deltaTime;
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		if (triggered == false && coll.gameObject.tag == "Player") {
+			triggered = true;
+			helpText.SetActive (false);
+		}
+	}
+}
