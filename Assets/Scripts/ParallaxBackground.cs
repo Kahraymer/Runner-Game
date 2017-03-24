2c4256246636ad373cb20c5bdb50edb4ccf2d462
Varun Ramesh <varunramesh@live.com>
2017-03-24 09:27:21 -0700

    Stop background scrolling when level ends.

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class ParallaxBackground : MonoBehaviour {
 	public float speed;
+	private LevelEnd levelEnd;
 
 	// Use this for initialization
 	void Start () {
-		
+		GameObject end = GameObject.FindGameObjectWithTag ("LevelEnd");
+		if (end != null)
+			levelEnd = end.GetComponent<LevelEnd> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (levelEnd != null && levelEnd.LevelEnded)
+			return;
 		transform.localPosition += Time.deltaTime * speed * new Vector3 (1, 0, 0);
 	}
 }
