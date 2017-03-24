eaecccdf58eb74ade613a0d3370858eb80da724f
Varun Ramesh <varunramesh@live.com>
2017-03-23 20:37:27 -0700

    Add a button to go back to the main menu from the world select.

---
+++ Assets/GoToMainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GoToMainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Go() {
+		SceneManager.LoadScene ("Start", LoadSceneMode.Single);
+		GameObject menuMusic = GameObject.FindGameObjectWithTag ("MenuMusic");
+		if (menuMusic) {
+			Destroy (menuMusic);
+		}
+	}
+}
